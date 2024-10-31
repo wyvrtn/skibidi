b50fabae9758ba73d4795fd8bcfeb433e2b8ba2e
Wyv <181348033+wyvrtn@users.noreply.github.com>
2024-10-31 10:15:35 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#define def left right 
+#define skibidi <iostream>
+#define sigma <string>
+#define rizz int
+#define rizzler main
+#define left (
+#define right )
+#define frontshots {
+#define backshots }
+#define HIV std
+
+#include skibidi
+#include sigma
+
+rizz rizzler left right
+frontshots
+  HIV::string name;
+  HIV::cout << "What is your name? ";
+  getline (HIV::cin, name);
+  HIV::cout << "Hello, " << name << "!\n";
+backshots
